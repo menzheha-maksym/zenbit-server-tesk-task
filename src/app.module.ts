bd54ea02fb8f2a83073dfd14df346d59764148b1
Maksym Menzheha <masimfyd@gmail.com>
2022-01-28 14:16:45 +0200

    add host to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,7 @@ import { ContactsModule } from './contacts/contacts.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
+      host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
